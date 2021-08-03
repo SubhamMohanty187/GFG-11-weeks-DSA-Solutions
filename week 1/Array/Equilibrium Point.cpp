6535ecb000d360675925a82693d92176e5b47c10
SubhamMohanty187 <87409528+SubhamMohanty187@users.noreply.github.com>
2021-08-03 12:54:58 +0530

    Create Equilibrium Point.cpp
    Given an array A of n positive numbers. The task is to find the first Equilibrium Point in the array. 
    Equilibrium Point in an array is a position such that the sum of elements before it is equal to the sum of elements after it.

---
+++ week 1/Array/Equilibrium Point.cpp
@@ -0,0 +1,18 @@
+int equilibriumPoint(long long a[], int n) {
+    
+        int i;
+        long long lsum=0,rsum=0;
+        for(i=0;i<n;i++)
+        {
+            rsum += a[i];
+        }
+        for(i=0;i<n;i++)
+        {
+            rsum-=a[i];
+            if(lsum == rsum)
+            return i+1;
+            
+            lsum+= a[i];
+        }
+        return -1;
+    }
