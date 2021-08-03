31cadbe389f10789fdde4d63053e2db1b5db3477
SubhamMohanty187 <87409528+SubhamMohanty187@users.noreply.github.com>
2021-08-03 12:46:27 +0530

    Create Modular Multiplicative Inverse.cpp
    Given two integers ‘a’ and ‘m’. The task is to find the smallest modular multiplicative inverse of ‘a’ under modulo ‘m’.

---
+++ week 1/Mathematics/Modular Multiplicative Inverse.cpp
@@ -0,0 +1,9 @@
+int modInverse(int a, int m)
+    {
+        for (int x = 1; x < m; x++)
+        {
+        if (((a%m) * (x%m)) % m == 1)
+            return x;
+        }
+        return -1;
+    }
