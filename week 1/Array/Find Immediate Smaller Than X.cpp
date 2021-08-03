8220026aeb5941c87387f53c59622095bfd819f6
SubhamMohanty187 <87409528+SubhamMohanty187@users.noreply.github.com>
2021-08-03 23:10:03 +0530

    Create Find Immediate Smaller Than X.cpp
    Given an array arr[] of size N containing positive integers and an integer X, find the element in the array which is smaller than X and closest to it.

---
+++ week 1/Array/Find Immediate Smaller Than X.cpp
@@ -0,0 +1,12 @@
+int immediateSmaller(int arr[], int n, int x)
+    {
+      
+        int rem=-1;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]>rem && arr[i]<x)
+            rem = arr[i];
+        }
+        return rem;
+        
+    }
