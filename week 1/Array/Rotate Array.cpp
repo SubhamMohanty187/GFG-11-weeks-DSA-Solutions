b0c939db443a603c00404e86fd090f5aa16d2700
SubhamMohanty187 <87409528+SubhamMohanty187@users.noreply.github.com>
2021-08-03 12:51:48 +0530

    Create Rotate Array.cpp
    Given an unsorted array arr[] of size N, rotate it by D elements in the counter-clockwise direction.

---
+++ week 1/Array/Rotate Array.cpp
@@ -0,0 +1,15 @@
+void rotateArr(int arr[], int d, int n){
+        // code here
+        reverse(arr,0,d-1);
+        reverse(arr,d,n-1);
+        reverse(arr,0,n-1);
+    }
+    void reverse(int a[],int l,int r)
+    {
+        while(l < r)
+        {
+            swap(a[l],a[r]);
+            l++;
+            r--;
+        }
+    }
