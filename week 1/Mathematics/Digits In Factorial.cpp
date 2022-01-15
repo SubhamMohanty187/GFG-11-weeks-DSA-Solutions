f4242a25a0a3c21d3063d08debfee00124bae3d8
SubhamMohanty187 <87409528+SubhamMohanty187@users.noreply.github.com>
2022-01-15 12:58:34 +0530

    Update Digits In Factorial.cpp

--- week 1/Mathematics/Digits In Factorial.cpp
+++ week 1/Mathematics/Digits In Factorial.cpp
@@ -4,7 +4,6 @@ int digitsInFactorial(int N)
         if(N==0 || N==1)
         return 1;
         
-        
        int x = ((N * log10(N / M_E) + log10(2 * M_PI * N) /2.0));
        
        return floor(x) + 1;
