1cf199e821536d9e2313f0e3b613cd7ddbd63372
Subham Mohanty <87409528+SubhamMohanty187@users.noreply.github.com>
2022-01-17 13:23:53 +0530

    Update Smallest Positive missing number.cpp

--- week 1/Array/Smallest Positive missing number.cpp
+++ week 1/Array/Smallest Positive missing number.cpp
@@ -1,7 +1,7 @@
 int findMissing(int arr[], int n) { 
         for (int i = 0; i < n; i++) 
         {
-       
+ 
           while (arr[i] >= 1 && arr[i] <= n && arr[i] != arr[arr[i] - 1]) 
           {
             swap(arr[i], arr[arr[i] - 1]);
